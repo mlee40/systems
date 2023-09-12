548c1bb674950ce20a8b8407bd2d7d4677316dca
Mary Lee <mlee40@stuy.edu>
2023-09-12 15:31:50 -0400

    9-12-23

--- hello.c
+++ hello.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int foo(int n){
+    return 'a';
 }
 int main(){
     foo(3);
     printf("Hello World\n");
+    printf("these are numbers: %d, %0.10lf\n", 345, 9838.12);
+    return 0;
 }
